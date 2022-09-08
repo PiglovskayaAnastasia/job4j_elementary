a681a75e2496ff3d4b2ac394901379ef71d28b61
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-09-08 19:53:54 +0300

    3.2. Загрузка файлов на github [#243562 #336292]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class Studentinfo {
-    public static void main (String[] args) {
+public class StudentInfo {
+    public static void main(String[] args) {
         System.out.println("Anastasia Piglovskaya");
         System.out.println("15.04.1989");
     }
