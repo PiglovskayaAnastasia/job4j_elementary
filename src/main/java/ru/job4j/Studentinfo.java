2d973324f2a74a25d5238d425716d1bf5b2458b4
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-09-08 19:34:48 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main (String[] args) {
+        System.out.println("Anastasia Piglovskaya");
+        System.out.println("15.04.1989");
+    }
+}
+
