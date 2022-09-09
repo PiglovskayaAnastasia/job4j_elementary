3cdf13e9a4c97949a97bd9035a750a8de73fa2e9
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-09-09 22:46:23 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("02.09.2022");
+
+        System.out.println("09.09.2022");
     }
 }
