fdb72f37dcdceb78461405f25e169dbd0305ccdd
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-09-13 13:28:46 +0300

    3.2.Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -4,7 +4,7 @@ public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String name = "Job4j";
         int age = 6;
         ArgMethod.hello(name,age);
