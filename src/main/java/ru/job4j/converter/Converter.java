5facab4d3575148bbee9ce7261e3201de81752d8
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-09-13 15:16:00 +0300

    3.2.Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,18 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return rsl;
+    }
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        float dollar = Converter.rubleToDollar(650);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("650 rubles are " + dollar + " dollar.");
+    }
+}
