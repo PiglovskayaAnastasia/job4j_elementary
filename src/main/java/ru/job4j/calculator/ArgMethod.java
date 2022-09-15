080c662614de24f344a6fb041aa41a18f8fb59ec
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-09-15 12:34:40 +0300

    4.1. Аргументы [#295521 #342419]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -11,5 +11,6 @@ public class ArgMethod {
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
     }
 }
