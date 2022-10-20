211a72a9988bdf2c9fcee476e33c612c92f36686
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-10-20 18:24:13 +0300

    5.5. Простое число [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        for (int i = 1; i <= number; i++) {
+            if (number % 2 == 0) {
+                prime = false;
+                break;
+            } else if (number == 1) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
