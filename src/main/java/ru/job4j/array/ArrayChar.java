7fd2dfd7d9e8d15cc6ce6c69a0eaa9fe982b84a2
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-11-02 09:52:35 +0300

    6.4.0. Слово начинается с ... [#41585 #356759]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,14 +3,8 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        int minlength = word.length;
-        if (word.length > pref.length) {
-            minlength = pref.length;
-        }
-        for (int i = 0; i < minlength; i++) {
-            if (word[i] == pref[i]) {
-                result = result;
-            } else {
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
                 result = false;
             }
         }
