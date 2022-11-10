7a3215349b4cff6f4f8811adbae5feaaf7dc53f0
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2022-11-10 11:36:03 +0300

    6.6.4. Двухмерный массив. Таблица умножения. [#33491]

--- src/test/java/ru/job4j/array/MatrixTest.java
+++ src/test/java/ru/job4j/array/MatrixTest.java
@@ -19,7 +19,7 @@ public class MatrixTest {
         int[][] result = Matrix.multiple(size);
         int[][] expected = {
                 {1, 2, 3, 4, 5},
-                {2, 4, 6, 8, 12},
+                {2, 4, 6, 8, 10},
                 {3, 6, 9, 12, 15},
                 {4, 8, 12, 16, 20},
                 {5, 10, 15, 20, 25}};
